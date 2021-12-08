2eb48b5940c392a45baf53a01b647f36060d59ec
Takashi Aoki <arfaben_5816@icloud.com>
2021-12-08 23:41:19 +0900

    配信開始日付を取得するロジックを実装

--- app/Services/Youtube/VideosListService.php
+++ app/Services/Youtube/VideosListService.php
@@ -2,6 +2,45 @@
 
 namespace App\Services\Youtube;
 
+use App\Repositories\Guzzle\GuzzleRepositoryInterface;
+use DateTime;
+
 class VideosListService
 {
+    public function __construct(GuzzleRepositoryInterface $clientInterface)
+    {
+        $this->clientInterface = $clientInterface;
+    }
+
+    public function getScheduledStartTime($videoId)
+    {
+        $method = "GET";
+
+        $url = $this->setUrl($videoId);
+        $response = $this->clientInterface->firstRequest($method, $url);
+
+        if ($response instanceof RequestException) {
+            //メインのAPIキーが使えなかった場合、別プロジェクトのAPIキーを使用
+            $url = $this->subSetUrl($videoId);
+            $response = $this->clientInterface->secondRequest($method, $url);
+        }
+
+        $body = $response->getBody();
+        $video = json_decode($body, true);
+
+        $date = $video["items"][0]["liveStreamingDetails"]["scheduledStartTime"];
+        $scheduleStartTime = new DateTime($date);
+
+        return $scheduleStartTime->format('Y-m-d');
+    }
+
+    public function setUrl($videoId)
+    {
+        return "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=" . $videoId . "&key=" . config('app.API_KEY');
+    }
+
+    public function subSetUrl($videoId)
+    {
+        return "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=" . $videoId . "&key=" . config('app.SUB_API_KEY');
+    }
 }
