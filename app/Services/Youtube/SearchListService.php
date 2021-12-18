d81d1a67607b6c6d306506fa3667d14ae3753d3e
Takashi Aoki <arfaben_5816@icloud.com>
2021-12-18 21:18:25 +0900

    配信予定の動画一覧を取得するロジックを実装

--- app/Services/Youtube/SearchListService.php
+++ app/Services/Youtube/SearchListService.php
@@ -13,7 +13,9 @@ class SearchListService
     protected $clientInterface;
 
     /**
+     * @param Member $member
      * @param GuzzleRepositoryInterface $clientInterface
+     * @param VideosListService $videosListService
      */
     public function __construct(Member $member, GuzzleRepositoryInterface $clientInterface, VideosListService $videosListService)
     {
@@ -35,7 +37,8 @@ class SearchListService
         $members = $this->member->getAllMembers();
         foreach ($members as $member) {
             $channelId = $member->channel_id;
-            $url = $this->setUrl($channelId);
+            // $url = $this->setUrl($channelId);
+            $url = $this->subSetUrl($channelId);
 
             $response = $this->clientInterface->firstRequest($method, $url);
 
@@ -54,36 +57,29 @@ class SearchListService
                     $videoId = $video["items"][0]["id"]["videoId"];
                     $interval = $this->videosListService->checkDiffToday($videoId);
 
-                    if ($interval <= "10") {
-                        $this->storageVideoInfo($video["items"][0], $video["regionCode"]);
+                    if ($interval <= "7") {
+                        $videoInfoArr = array();
+                        $videoInfo = $this->storageVideoInfo($video["items"][0], $video["regionCode"]);
+                        array_push($videoInfoArr, $videoInfo);
+                        $videos = $this->storageVideoInfoArrToVideos($videos, $videoInfoArr);
                     }
                 }
 
                 // 配信予定が複数ある場合
                 if (count($video["items"]) > "1") {
                     $videoIds = $this->storageVideoIds($video["items"]);
-                    $videoInfoArr = array();
-                    foreach ($videoIds as $key => $videoId) {
-                        $interval = $this->videosListService->checkDiffToday($videoId);
-
-                        if ($interval <= "10") {
-                            dd($video);
-                            $videoInfo = $this->storageVideoInfo($video["items"][$key], $video["regionCode"]);
-                            array_push($videoInfoArr, $videoInfo);
-                            dd($videoInfoArr);
-                        }
-                    }
+                    $videoInfoArr = $this->storageVideoInfoArr($videoIds, $video);
+                    $videos = $this->storageVideoInfoArrToVideos($videos, $videoInfoArr);
                 }
             }
         }
-        dd($videos);
         $this->storeDailyUpcomingVideos($videos);
     }
 
     /**
      * メインAPIキーを使用
      *
-     * @param int $channelId
+     * @param string $channelId
      * @return string
      */
     public function setUrl($channelId)
@@ -102,6 +98,11 @@ class SearchListService
         return "https://www.googleapis.com/youtube/v3/search?key=" . config('app.SUB_API_KEY') . "&channel_id=" . $channelId . "&part=snippet&eventType=upcoming&type=video";
     }
 
+    /**
+     * 複数の動画IDを$videoIdsに格納
+     *
+     * @return array $videoIds
+     */
     public function storageVideoIds($items)
     {
         $videoIds = array();
@@ -113,14 +114,42 @@ class SearchListService
         return $videoIds;
     }
 
+    /**
+     * 複数の動画情報を$videoInfoArrに格納
+     *
+     * @param array $videoIds
+     * @param array $video
+     */
+    public function storageVideoInfoArr($videoIds, $video)
+    {
+        $videoInfoArr = array();
+        foreach ($videoIds as $key => $videoId) {
+            $interval = $this->videosListService->checkDiffToday($videoId);
+
+            if ($interval <= "7") {
+                $videoInfo = $this->storageVideoInfo($video["items"][$key], $video["regionCode"]);
+                array_push($videoInfoArr, $videoInfo);
+            }
+        }
+
+        return $videoInfoArr;
+    }
+
+    /**
+     * 動画情報を$videoInfoに格納
+     *
+     * @param array $video
+     * @param string $regionCode
+     * @return array $videoInfo
+     */
     public function storageVideoInfo($video, $regionCode)
     {
         $videoInfo = array();
+
         $videoId = $video["id"]["videoId"];
         $channelId = $video["snippet"]["channelId"];
         $title = $video["snippet"]["title"];
         $thumbnails = $video["snippet"]["thumbnails"]["medium"]["url"];
-
         $date = $this->videosListService->getScheduledStartTime($videoId);
         $scheduleStartTime = $date->format('Y-m-d');
 
@@ -130,15 +159,15 @@ class SearchListService
     }
 
     /**
-     * Undocumented function
+     * 複数の配信情報を配信予定動画一覧（$videos）に格納
      *
      * @param array $videos
-     * @param array $video
+     * @param array $videoInfoArr
      * @return array
      */
-    public function storageVideos($videos, $video)
+    public function storageVideoInfoArrToVideos($videos, $videoInfoArr)
     {
-        return array_merge($videos, $video);
+        return array_merge($videos, $videoInfoArr);
     }
 
     public function storeDailyUpcomingVideos($videos)
