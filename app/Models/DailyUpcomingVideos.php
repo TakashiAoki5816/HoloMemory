400e84d2417f7dd70f02e0078d6310802faf5259
Takashi Aoki <arfaben_5816@icloud.com>
2022-01-21 16:11:42 +0900

    MemberとDailyUpcomingVideosのリレーションを設定

--- app/Models/DailyUpcomingVideos.php
+++ app/Models/DailyUpcomingVideos.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Member;
 use Illuminate\Database\Eloquent\Model;
 
 class DailyUpcomingVideos extends Model
 {
     protected $table = 'daily_upcoming_videos';
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'channel_id', 'channel_id');
+    }
 
     /**
      * 登録されている配信予定動画を全て取得
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\DailyUpcomingVideos;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
     protected $table = 'HOLOLIVE_MEMBER_MST';
+
+    public function daily_upcoming_videos()
+    {
+        return $this->hasMany(DailyUpcomingVideos::class);
+    }
 
     /**
      * 所属している全メンバーを取得
