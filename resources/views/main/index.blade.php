e98eaf4c1b119933496c248ffc253e4386c237d4
Takashi Aoki <arfaben_5816@icloud.com>
2022-02-09 23:39:04 +0900

    ヘッダーデザイン作成

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -18,7 +18,12 @@
 <body>
     <div class="w-full">
         <header class="header">
-            <h1>HoloMemory</h1>
+            <button>
+                <svg class="hamburger" viewBox="0 0 24 24">
+                    <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+                </svg>
+            </button>
+            <h1 class="logo">HoloMemory</h1>
         </header>
         <div class="container m-auto">
 
