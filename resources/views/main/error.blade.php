0008ebd4ce31c7147b6a3afde282af0c28ad9471
Takashi Aoki <arfaben_5816@icloud.com>
2022-02-22 14:51:26 +0900

    エラーページのビュー作成

---
+++ resources/views/main/error.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer="defer"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    @if ($message)
+    <p class="mt-5 font-bold text-red-600">
+        {{ $message }}
+    </p>
+    @endif
+</body>
+
+</html>
