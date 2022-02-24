bf32b0f8d88ad6d6fda326da11e580aa958d86e7
Takashi Aoki <arfaben_5816@icloud.com>
2022-02-24 14:07:39 +0900

    [Fix]例外発生時はExceptionクラスをcatchするように修正

--- app/Repositories/Guzzle/GuzzleRepository.php
+++ app/Repositories/Guzzle/GuzzleRepository.php
@@ -5,9 +5,9 @@ namespace App\Repositories\Guzzle;
 use App\Consts\GuzzleRepositoryConsts;
 use App\Exceptions\RedirectExceptions;
 use App\Repositories\Guzzle\GuzzleRepositoryInterface;
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Exception\ClientException;
 use Illuminate\Support\Facades\Log;
 
 class GuzzleRepository implements GuzzleRepositoryInterface
@@ -22,13 +22,12 @@ class GuzzleRepository implements GuzzleRepositoryInterface
      *
      * @param string $url
      * @return Response
-     * @return Exception $e
      */
-    public function firstRequest($url)
+    public function firstRequest(string $url)
     {
         try {
             return $this->client->request(GuzzleRepositoryConsts::GET_METHOD, $url);
-        } catch (ClientException $e) {
+        } catch (Exception $e) {
             return $e;
         }
     }
@@ -40,11 +39,11 @@ class GuzzleRepository implements GuzzleRepositoryInterface
      * @return Response
      * @throws RedirectExceptions
      */
-    public function secondRequest($url)
+    public function secondRequest(string $url)
     {
         try {
             return $this->client->request(GuzzleRepositoryConsts::GET_METHOD, $url);
-        } catch (ClientException $e) {
+        } catch (Exception $e) {
             Log::debug("エラー内容:" . $e->getMessage() . PHP_EOL . "ファイル名:" . $e->getFile() . PHP_EOL . "エラー行:" . $e->getLine());
             throw new RedirectExceptions(route('root'), "1日のクォータ使用量を超えているため、リクエストを完了できません。", $e->getCode());
         }
