afb98a740ef58c29b9cb8523ed2ca482f3d78207
Takashi Aoki <arfaben_5816@icloud.com>
2022-03-03 15:17:54 +0900

    UPDATE:viewを返すだけに更新

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
-use App\Services\Youtube\SearchListService;
+use App\Models\DailyUpcomingVideos;
 use Illuminate\Http\RedirectResponse;
+use App\Services\Youtube\SearchListService;
 
 class YoutubeController extends Controller
 {
@@ -12,25 +13,70 @@ class YoutubeController extends Controller
     protected $searchListService;
 
     /**
-     * @param Member $member
      * @param SearchListService $searchListService
      */
-    public function __construct(Member $member, SearchListService $searchListService)
+    public function __construct(SearchListService $searchListService, DailyUpcomingVideos $dailyUpcomingVideos)
     {
-        $this->member = $member;
         $this->searchListService = $searchListService;
+        $this->dailyUpcomingVideos = $dailyUpcomingVideos;
     }
+
+    /**
+     * 配信情報一覧取得
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $videos = $this->dailyUpcomingVideos->getVideos();
+
+        return response()->json($videos);
+    }
+
+    /** */
+    // public function getVideos(): RedirectResponse
+    // {
+    //     $members = $this->member->getAllMembers();
+
+    //     return redirect()->route('root');
+    // }
 
     /**
      * 最新の配信予定動画一覧を取得
      *
      * @return RedirectResponse
      */
-    public function request(): RedirectResponse
+    public function store()
     {
-        $members = $this->member->getAllMembers();
-        $this->searchListService->requestSearchList($members);
+        $this->searchListService->requestSearchList();
+        $videos = $this->dailyUpcomingVideos->getVideos();
 
-        return redirect()->route('root');
+        return response()->json($videos);
+    }
+
+    # 投稿表示
+    public function show(Int $id)
+    {
+        $post = Post::find($id);
+        return response()->json($post);
+    }
+
+
+    # 投稿編集
+    public function update(Int $id, Request $request)
+    {
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->updated_at = now();
+        $post->save();
+        return response()->json($post);
+    }
+
+    # 投稿削除
+    public function delete(Int $id)
+    {
+        $post = Post::find($id)->delete();
+        return response()->json(Post::all());
     }
 }
