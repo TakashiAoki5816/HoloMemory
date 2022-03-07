1f0e2ead1556248df179b891a393dc861b452dc0
Takashi Aoki <arfaben_5816@icloud.com>
2022-03-07 15:42:57 +0900

    CHANGE:app.blade.phpをコンポーネント化に更新

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,69 +17,10 @@
 
 <body>
     <div id="app">
-        <example-component></example-component>
-    </div>
-    <div class="w-full">
-        <header class="header">
-            <button>
-                <svg class="hamburger" viewBox="0 0 24 24">
-                    <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
-                </svg>
-            </button>
-            <h1 class="logo">HoloMemory</h1>
-        </header>
-        <div class="container m-auto">
-            <div class="flex justify-between">
-                <div class="notification is-danger">
-                    @if ($errors->has('exception'))
-                    <p class="mt-5 font-bold text-red-600">{{ $errors->first('exception') . "(" .
-                        $errors->first('statusCode') .
-                        "エラー)" }}</p>
-                    @endif
-                </div>
-                <div class="request-box">
-                    <form method="GET" action="{{ route('youtube.request') }}">
-                        <button class="request-button" type="submit">
-                            最新の配信情報を取得
-                        </button>
-                    </form>
-                </div>
-            </div>
-            <main>
-                @if (isset($videos))
-                <div class="lessons">
-                    @foreach ($videos as $key => $video)
-                    {{-- 日付の境界線チェック --}}
-                    @if ($key === 0 || substr($videos[$key]["scheduled_start_time"], 5, 5) !=
-                    substr($videos[$key-1]["scheduled_start_time"], 5, 5))
-                    <div class="date-section">
-                        <h2 class="text-white text-center leading-[60px]">{{ $video->start_date }}</h2>
-                    </div>
-                    @endif
-                    <div class="lesson">
-                        <div class="lesson-header">
-                            <div>{{ $video->start_time }}</div>
-                            <div>{{ $video->member->name }}</div>
-                        </div>
-                        <div class="lesson-image">
-                            <a href="https://www.youtube.com/watch?v={{$video->video_id }}" target="_blank"
-                                rel="noopener noreferrer">
-                                <img src="{{ $video['thumbnails_url'] }}">
-                            </a>
-                        </div>
-                        <div>
-                            <a class="lesson-channel-icon"
-                                href="https://www.youtube.com/channel/{{ $video->member->channel_id}}" target="_blank"
-                                rel="noopener noreferrer">
-                                <img src="{{ $video->member->channel_icon_url }}"
-                                    style="border-radius: 50%; border: 2px#eeac5e solid" width="60" height="60">
-                            </a>
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-                @endif
-            </main>
+        <div class="w-full">
+            <header-component></header-component>
+            <container-component :errors="{{ $errors }}">
+            </container-component>
         </div>
     </div>
 </body>
