7b86658f1579dd28b34905ef95dbb2cb6e059353
Takashi Aoki <arfaben_5816@icloud.com>
2022-03-12 12:38:46 +0900

    UPDATE:アクセサで取得したデータを表示、最新の配信情報をaxiosから取得

--- resources/js/components/ContainerComponent.vue
+++ resources/js/components/ContainerComponent.vue
@@ -7,7 +7,7 @@
                 </strong>
             </div>
             <div class="request-box">
-                <form method="GET" action="/api/videos/store">
+                <form v-on:submit.prevent="submit">
                     <button class="request-button" type="submit">
                         最新の配信情報を取得
                     </button>
@@ -15,64 +15,66 @@
             </div>
         </div>
         <main>
-            <!-- @if (isset($videos)) -->
-            <div class="lessons">
-                <!-- 日付の境界線チェック -->
-                <!-- @if ($key === 0 || substr($videos[$key]["scheduled_start_time"],
-                5, 5) != substr($videos[$key-1]["scheduled_start_time"], 5, 5)) -->
-                <!-- <div class="date-section">
-                    <h2 class="text-white text-center leading-[60px]">
-                        {{ $video.start_date }}
-                    </h2>
-                </div> -->
-                <!-- @endif -->
-
-                <div
-                    class="lesson"
-                    v-for="(video, index) in videos"
-                    :key="index"
-                >
-                    <div class="lesson-header">
-                        <div>{{ video.start_time }}</div>
-                        <div>{{ video.member.name }}</div>
-                    </div>
-                    <div class="lesson-image">
-                        <a
-                            v-bind:href="
-                                'https://www.youtube.com/watch?v=' +
-                                video.video_id
+            <div class="container">
+                <ul>
+                    <li v-for="(video, index) in videos" :key="index">
+                        <div
+                            class="date-section"
+                            v-if="
+                                index === 0 ||
+                                video.start_date != videos[index - 1].start_date
                             "
-                            target="_blank"
-                            rel="noopener
+                        >
+                            <h2 class="text-white text-center leading-[60px]">
+                                {{ video.start_date }}
+                            </h2>
+                        </div>
+                        <div class="lesson">
+                            <div class="lesson-header">
+                                <div>{{ video.start_time }}</div>
+                                <div>{{ video.member.name }}</div>
+                            </div>
+                            <div class="lesson-image">
+                                <a
+                                    v-bind:href="
+                                        'https://www.youtube.com/watch?v=' +
+                                        video.video_id
+                                    "
+                                    target="_blank"
+                                    rel="noopener
                             noreferrer"
-                        >
-                            <img v-bind:src="video.thumbnails_url" />
-                        </a>
-                    </div>
-                    <div>
-                        <a
-                            class="lesson-channel-icon"
-                            v-bind:href="
-                                'https://www.youtube.com/channel/' +
-                                video.member.channel_id
-                            "
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <img
-                                v-bind:src="video.member.channel_icon_url"
-                                style="
-                                    border-radius: 50%;
-                                    border: 2px#eeac5e solid;
-                                "
-                                width="60"
-                                height="60"
-                            />
-                        </a>
-                    </div>
-                </div>
+                                >
+                                    <img v-bind:src="video.thumbnails_url" />
+                                </a>
+                            </div>
+                            <div>
+                                <a
+                                    class="lesson-channel-icon"
+                                    v-bind:href="
+                                        'https://www.youtube.com/channel/' +
+                                        video.member.channel_id
+                                    "
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <img
+                                        v-bind:src="
+                                            video.member.channel_icon_url
+                                        "
+                                        style="
+                                            border-radius: 50%;
+                                            border: 2px#eeac5e solid;
+                                        "
+                                        width="60"
+                                        height="60"
+                                    />
+                                </a>
+                            </div>
+                            <!-- </div> -->
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <!-- @endif -->
         </main>
     </div>
 </template>
@@ -92,7 +94,14 @@ export default {
     methods: {
         getVideos() {
             axios.get("api/videos").then((res) => {
+                console.log(res.data);
                 this.videos = res.data;
+            });
+        },
+        submit() {
+            axios.get("/api/videos/create").then(() => {
+                // this.$router.push("/");
+                this.getVideos();
             });
         },
     },
