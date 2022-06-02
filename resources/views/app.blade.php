f863aebbf7529a4276b29d62834761ace7ab017a
Takashi Aoki <arfaben_5816@icloud.com>
2022-06-02 23:41:31 +0900

    Change:titleを変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'HoloMemory') }}</title>
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer="defer"></script>
     <!-- Fonts -->
