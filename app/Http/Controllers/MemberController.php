35dd4ee8533ba4444d82b1080288584af122b65b
Takashi Aoki <arfaben_5816@icloud.com>
2022-07-12 20:49:52 +0900

    Create:MemberのController, view, Componentを作成

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
     public function index()
     {
+        return view('member');
     }
 }
