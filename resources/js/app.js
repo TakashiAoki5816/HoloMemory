ba9fdaa253ae8496ad309a5b29400fdcdab6516e
Takashi Aoki <arfaben_5816@icloud.com>
2022-08-12 00:18:19 +0900

    Add:font-awesome導入、メンバーボックスのデザインを作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,27 +5,27 @@ import HeaderComponent from "./components/Main/HeaderComponent.vue";
 import ContainerComponent from "./components/Main/ContainerComponent.vue";
 import MembersComponent from "./components/Main/MemberComponent.vue";
 
-// const routes = [
-//     {
-//         path: "/",
-//         name: "Home",
-//         component: Home,
-//     },
-// ];
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes,
+library.add(fas, far);
+
+const app = createApp({});
+app.component("header-component", HeaderComponent);
+app.component("container-component", ContainerComponent);
+app.component("members-component", MembersComponent);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
+
+// const app = createApp({
+//     components: {
+//         HeaderComponent,
+//         ContainerComponent,
+//         MembersComponent,
+//         FontAwesomeIcon,
+//     },
 // });
 
-const app = createApp({
-    components: {
-        HeaderComponent,
-        ContainerComponent,
-        MembersComponent,
-    },
-});
-
-// app.use(router);
-
-app.mount("#app");
+// app.mount("#app");
--- resources/js/components/Main/MemberComponent.vue
+++ resources/js/components/Main/MemberComponent.vue
@@ -1,11 +1,40 @@
 <template>
-    <div class="container m-auto bg-teal-300">
+    <div class="w-2/3 m-auto">
         <div class="mt-5">
             <select class="select-group">
                 <option value="ALL">全て</option>
             </select>
         </div>
-        <div class="member-box"></div>
+        <div class="generation-container">
+            <div class="generation-box">
+                <div class="generation-line">
+                    <h2 class="generation-text">1期生</h2>
+                </div>
+            </div>
+            <div class="member-box">
+                <div class="member-left-box">
+                    <div class="member-image-box">
+                        <img
+                            class="member-image"
+                            src="../../../../public/images/kazama.jpeg"
+                        />
+                    </div>
+                    <div class="member-name-box">
+                        <p class="member-name">風間いろは</p>
+                    </div>
+                </div>
+                <div class="member-right-box">
+                    <div class="member-like-box">
+                        <div class="member-like-button">
+                            <font-awesome-icon
+                                class="member-like-icon"
+                                :icon="['fas', 'star']"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
