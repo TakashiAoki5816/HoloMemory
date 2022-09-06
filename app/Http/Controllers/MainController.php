0d40e73b788a8dfcb27911930779a5f432a54b37
Takashi Aoki <arfaben_5816@icloud.com>
2022-09-07 00:14:13 +0900

    update:ディレクティブを使ったレイアウトに変更

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,6 +13,6 @@ class MainController extends Controller
      */
     public function main(): View
     {
-        return view('layouts.app');
+        return view('layouts.main');
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<members-component></members-component>
+<container-component></container-component>
 @endsection
