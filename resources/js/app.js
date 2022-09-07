ed54b096f9db6a4c070b439e540a4bd8aa5ad9e2
Takashi Aoki <arfaben_5816@icloud.com>
2022-09-08 00:51:00 +0900

    rename:ContainerComponentをStreamScheduleComponentに名前を変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ require("./bootstrap");
 import { createApp } from "vue";
 // import { createRouter, createWebHistory } from "vue-router";
 import HeaderComponent from "./components/Main/HeaderComponent.vue";
-import ContainerComponent from "./components/Main/ContainerComponent.vue";
+import StreamScheduleComponent from "./components/Main/StreamScheduleComponent.vue";
 import MembersComponent from "./components/Main/MemberComponent.vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -14,7 +14,7 @@ library.add(fas, far);
 
 const app = createApp({});
 app.component("header-component", HeaderComponent);
-app.component("container-component", ContainerComponent);
+app.component("stream-schedule-component", StreamScheduleComponent);
 app.component("members-component", MembersComponent);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
@@ -22,7 +22,7 @@ app.mount("#app");
 // const app = createApp({
 //     components: {
 //         HeaderComponent,
-//         ContainerComponent,
+//         StreamScheduleComponent,
 //         MembersComponent,
 //         FontAwesomeIcon,
 //     },
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<container-component></container-component>
+<stream-schedule-component></stream-schedule-component>
 @endsection
